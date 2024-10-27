1b9729538a1fb732fbd19d327565a44ddba25fe6
kauawerle <kauawerle19@gmail.com>
2024-10-27 13:15:23 -0300

    feat: implement new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './layouts/main/main.component';
+import { LoginComponent } from './layouts/login/login.component';
+import { RegisterComponent } from './layouts/register/register.component';
+import { SendFormComponent } from './layouts/send-form/send-form.component';
+import { SendDocumentsComponent } from './layouts/send-documents/send-documents.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  {
+    path: 'sign-in',
+    component: LoginComponent,
+    children: [
+      {
+        path: 'sign-up', component: RegisterComponent
+      },
+      { path: 'send-form', component: SendFormComponent },
+      { path: 'send-documents', component: SendDocumentsComponent },
+    ]
+  }
+];
