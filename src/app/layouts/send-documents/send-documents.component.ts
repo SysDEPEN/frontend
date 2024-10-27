78efebb3e42d808373133e1893623397a6395daf
kauawerle <kauawerle19@gmail.com>
2024-10-27 10:05:46 -0300

    feat:create pages and components

---
+++ src/app/layouts/send-documents/send-documents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-documents',
+  standalone: true,
+  imports: [],
+  templateUrl: './send-documents.component.html',
+  styleUrl: './send-documents.component.scss'
+})
+export class SendDocumentsComponent {
+
+}
---
+++ src/app/layouts/send-form/send-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './send-form.component.html',
+  styleUrl: './send-form.component.scss'
+})
+export class SendFormComponent {
+
+}
