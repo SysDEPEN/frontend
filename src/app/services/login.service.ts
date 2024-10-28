d7789ceea4a2a7d233af0e84162fa54c93bee965
Guilherme Philippsen de Souza <guilhermephilippsen2003@gmail.com>
2024-10-28 20:53:45 -0300

    second commit

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login } from '../models/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  http = inject(HttpClient);
+
+  API = "http://localhost:8080/api/v1/logins"
+
   constructor() { }
+
+  handleLogin(login: Login): Observable<string> {
+    return this.http.post<string> (this.API+"/login")zz
+  }
 }
