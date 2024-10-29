315367a89acf04840039bf9bc2c871cc8d8bef66
mateco <riosmachadomatheus@gmail.com>
2024-10-29 20:47:47 -0300

    configuracao do app routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,26 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './layouts/main/main.component';
 import { LoginComponent } from './layouts/login/login.component';
 import { RegisterComponent } from './layouts/register/register.component';
 import { SendFormComponent } from './layouts/send-form/send-form.component';
 import { SendDocumentsComponent } from './layouts/send-documents/send-documents.component';
+import { NgModule } from '@angular/core';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: MainComponent },
+  { path: 'home', component: HomePageComponent},
   { path: 'sign-in', component: LoginComponent },
   { path: 'sign-up', component: RegisterComponent },
   { path: 'send-form', component: SendFormComponent },
   { path: 'send-document', component: SendDocumentsComponent },
+//  { path: 'documentos', component: DocumentosComponent }, // Crie o componente DocumentosComponent
+//  { path: 'sobre', component: SobreComponent }, // Crie o componente SobreComponent
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
