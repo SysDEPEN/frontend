2d3814ee5550428287c4cba1670180f57cbde6a9
Kauã Werle <kauawerle19@gmail.com>
2024-10-30 19:51:18 -0300

    feat:implement new service

---
+++ src/app/models/req_camps.ts
@@ -0,0 +1,31 @@
+export class reqCamp {
+  nome_visitado!: string;
+  cpf_rne!: string;
+  tipo_visitacao!: string;
+  telefone!: string;
+  estado!: string;
+  cidade!: string;
+  bairro!: string;
+  rua!: string;
+  numero_casa!: string;
+  requerimento_assunto!: string;
+  id_user!: {
+    id: number
+  }
+}
+
+// {
+//     "nome_visitado": "João Silva",
+//     "cpf_rne": "123.456.789-00",
+//     "tipo_visitacao": "Visitação Familiar",
+//     "telefone": "99999-9999",
+//     "estado": "SP",
+//     "cidade": "São Paulo",
+//     "bairro": "Centro",
+//     "rua": "Rua A",
+//     "numero_casa": "123",
+//     "requerimento_assunto": "Assunto do requerimento",
+//     "id_user": {
+//         "id": 1
+//     }
+// }
---
+++ src/app/services/req_camp.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { reqCamp } from '../models/req_camps';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ReqCampService {
+  http = inject(HttpClient);
+  API = 'http://localhost:8080/api/v1/req_camp';
+
+  constructor() {}
+
+  save(res: reqCamp): Observable<reqCamp> {
+    const req = res;
+    const url = `${this.API}/`;
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'secret-key':
+        'BvPHGM8C0ia4uOuxxqPD5DTbWC9F9TWvPStp3pb7ARo0oK2mJ3pd3YG4lxA9i8bj6OTbadwezxgeEByY',
+    });
+
+    return this.http.post<reqCamp>(url, req, { headers });
+  }
+}
