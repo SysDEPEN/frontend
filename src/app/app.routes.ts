cbd5c6b5493bf98edfe80483222ec9256544248d
mateco <riosmachadomatheus@gmail.com>
2024-10-30 21:14:40 -0300

    alteracao do layout do footer e header e implementacao da pagina sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './layouts/main/main.component';
 import { LoginComponent } from './layouts/login/login.component';
 import { RegisterComponent } from './layouts/register/register.component';
 import { SendFormComponent } from './layouts/send-form/send-form.component';
 import { SendDocumentsComponent } from './layouts/send-documents/send-documents.component';
 import { NgModule } from '@angular/core';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomePageComponent } from './layouts/home-page/home-page.component';
+import { SobreComponent } from './layouts/sobre/sobre.component';
 
 
 export const routes: Routes = [
@@ -16,7 +16,7 @@ export const routes: Routes = [
   { path: 'send-form', component: SendFormComponent },
   { path: 'send-document', component: SendDocumentsComponent },
 //  { path: 'documentos', component: DocumentosComponent }, 
-//  { path: 'sobre', component: SobreComponent }, 
+  { path: 'sobre', component: SobreComponent }, 
 ];
 
 @NgModule({
