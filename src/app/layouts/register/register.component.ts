06c04b96ad942f9ff0912660b0d57e80cd9b7576
Kauã Werle <kauawerle19@gmail.com>
2024-10-30 21:03:48 -0300

    create new inputs

--- src/app/layouts/register/register.component.ts
+++ src/app/layouts/register/register.component.ts
@@ -1,60 +1,91 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { RegisterService } from '../../services/register/register.service';
-import Swal from 'sweetalert2';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import Swal from 'sweetalert2';
+import { DatePickerComponent } from '../../components/date-picker/date-picker.component';
+import { RegisterService } from '../../services/register/register.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MdbFormsModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    MdbFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DatePickerComponent,
+  ],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
   router = inject(Router);
-
 
   constructor(public register: RegisterService) {
     this.registerForm = new FormGroup({
       name: new FormControl('', Validators.required),
       document: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
   }
 
   onRegister() {
     if (this.registerForm.valid) {
       const { name, document, email, password } = this.registerForm.value;
-      this.register.handleRegister(name, document, email, password).subscribe({
-        next: (response) => {
-          Swal.fire({
-            title: 'Sucesso!',
-            text: 'Cadastro realizado com sucesso',
-            icon: 'success',
-            confirmButtonText: 'Seguir para o Login'
-          });
-          this.router.navigate(['/login']);
-        },
-        error: (error) => {
-          Swal.fire({
-            title: 'Erro',
-            text: 'Falha ao realizar cadastro',
-            icon: 'error',
-            confirmButtonText: 'Tente novamente'
-          });
-        }
-      });
+      this.register
+        .handleRegister(
+          name,
+          document,
+          email,
+          password,
+          this.onBirthDateChange.toString()
+        )
+        .subscribe({
+          next: (response) => {
+            Swal.fire({
+              title: 'Sucesso!',
+              text: 'Cadastro realizado com sucesso',
+              icon: 'success',
+              confirmButtonText: 'Seguir para o Login',
+            });
+            this.router.navigate(['/login']);
+          },
+          error: (error) => {
+            Swal.fire({
+              title: 'Erro',
+              text: 'Falha ao realizar cadastro',
+              icon: 'error',
+              confirmButtonText: 'Tente novamente',
+            });
+          },
+        });
     } else {
       Swal.fire({
         title: 'Formulário inválido',
         text: 'Por favor, preencha todos os campos corretamente.',
         icon: 'warning',
-        confirmButtonText: 'OK'
+        confirmButtonText: 'OK',
       });
     }
+  }
+  selectedBirthDate: string = '';
+  selectedGender: string = '';
+  dateBirthday!: string;
+  gender!: string;
+
+
+  onBirthDateChange(date: string) {
+    this.selectedBirthDate = date;
+    console.log('Data de nascimento recebida:', date);
+  }
+
+  onGenderChange(gender: string) {
+    this.selectedGender = gender;
+    console.log('Gênero recebido:', gender);
   }
 }
--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -3,19 +3,35 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegisterService {
-
   private readonly API = 'http://localhost:8080/api/v1/users';
   private http = inject(HttpClient);
 
   constructor() {}
 
-  handleRegister(name: string, document: string, email: string, password: string): Observable<any> {
-    const registerData = { name, document, email, password };
+  handleRegister(
+    name: string,
+    document: string,
+    email: string,
+    password: string,
+    data_birth: string
+  ): Observable<any> {
+    const role:number = 0;
+    const registerData = {
+      name,
+      document,
+      email,
+      password,
+      data_birth,
+      role,
+      created_at: Date.now(),
+      updated_at: Date.now()
+    };
+
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
     return this.http.post(`${this.API}/save`, registerData, { headers });
   }
