4d2d74ffff26909890d56e60311b584d935887cc
Nuci <eloyjaci202@hotmail.com>
2024-12-05 00:59:59 -0300

    card protocolo

---
+++ src/app/layouts/nomedapagina/nomedapagina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nomedapagina',
+  standalone: true,
+  imports: [],
+  templateUrl: './nomedapagina.component.html',
+  styleUrl: './nomedapagina.component.scss'
+})
+export class NomedapaginaComponent {
+
+}
